25832f81f0dc9c92ed66587760944be521d1b88d
Ahuva <ahuvanewoff112@gmail.com>
2016-02-28 01:31:16 -0500

    changes

--- src/main/java/glazer/neuhoff/bejeweled/glazer/neuhoff/bejeweled/BejeweledFrame.java
+++ src/main/java/glazer/neuhoff/bejeweled/glazer/neuhoff/bejeweled/BejeweledFrame.java
@@ -1,18 +1,20 @@
 package glazer.neuhoff.bejeweled.glazer.neuhoff.bejeweled;
 
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 
 public class BejeweledFrame extends JFrame {
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	private Container container;
+	private JLabel[][] grid;
 	private ImageIcon frameIcon;
 	private Image img;
 
@@ -24,7 +26,18 @@ public class BejeweledFrame extends JFrame {
 		this.img = frameIcon.getImage().getScaledInstance(250, 250,
 				java.awt.Image.SCALE_SMOOTH);
 		setIconImage(img);
-		this.container = getContentPane();
+		container = getContentPane();
 		container.setLayout(new GridLayout(8, 8));
+		
+		add(container);
+		grid = new JLabel[8][8];
+		for (int row = 0; row < grid.length; row++) {
+			for (int col = 0; col < grid[row].length; col++) {
+				grid[row][col] = new JLabel();
+				//grid[row][col].setBackground(new Color(0, 0, 0, 0));
+				container.add(grid[row][col]);
+			}
+		}
+		
 	}
 }
