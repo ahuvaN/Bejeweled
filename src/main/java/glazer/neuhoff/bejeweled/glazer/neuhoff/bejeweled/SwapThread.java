34f3f0001257507bec9b7af714ab9d3470274bbd
unknown <chayalaglazer@gmail.com>
2016-03-13 23:59:59 -0400

    Fixed formatting

--- src/main/java/glazer/neuhoff/bejeweled/glazer/neuhoff/bejeweled/SwapThread.java
+++ src/main/java/glazer/neuhoff/bejeweled/glazer/neuhoff/bejeweled/SwapThread.java
@@ -12,7 +12,7 @@ this.grid=grid;
 	public void run(){
 		grid.swap(pressedLabel, enteredLabel);
 		try {
-			Thread.sleep(500);
+			Thread.sleep(300);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
